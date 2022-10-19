908d69971158c987a237233001fbd56373224672
VMO2020 <victor.ottati@gmail.com>
2022-10-19 11:26:48 +0100

    FEAT: Add Resize Image Link

--- src/App.js
+++ src/App.js
@@ -1,134 +1,146 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 // Components
-import { Share } from './components/Share';
+import { Share } from "./components/Share";
 
 // Icons
-import { ReactComponent as Icon1 } from './assets/icons/image.svg';
+import { ReactComponent as Icon1 } from "./assets/icons/image.svg";
 
 // Styles
-import './app.scss';
+import "./app.scss";
 
 // Library
-import exifr from 'exifr'; // => exifr/dist/full.umd.cjs
+import exifr from "exifr"; // => exifr/dist/full.umd.cjs
 
 function App() {
-	const [previewIMG, setPreviewIMG] = useState();
-	const [dataValue, setDataValue] = useState('');
-	const [img, setIMG] = useState('');
-	const [imgName, setImgName] = useState('');
-	const [imgSize, setImgSize] = useState('');
+  const [previewIMG, setPreviewIMG] = useState();
+  const [dataValue, setDataValue] = useState("");
+  const [img, setIMG] = useState("");
+  const [imgName, setImgName] = useState("");
+  const [imgSize, setImgSize] = useState("");
 
-	// INPUT IMAGE
-	const handleInputChange = (e) => {
-		const reset = '';
-		setPreviewIMG(reset);
+  // INPUT IMAGE
+  const handleInputChange = (e) => {
+    const reset = "";
+    setPreviewIMG(reset);
 
-		// Get the image selected mobile friendly
-		setIMG(URL.createObjectURL(e.target.files[0]));
+    // Get the image selected mobile friendly
+    setIMG(URL.createObjectURL(e.target.files[0]));
 
-		// Get the image selected
-		const file = document.querySelector('#uploader').files[0];
-		// const file = (e.target.files[0]);
+    // Get the image selected
+    const file = document.querySelector("#uploader").files[0];
+    // const file = (e.target.files[0]);
 
-		// Create a FileReader (Object)
-		const reader = new FileReader();
+    // Create a FileReader (Object)
+    const reader = new FileReader();
 
-		if (file) {
-			reader.readAsDataURL(file);
-			reader.name = file.name; //get the image's name
-			reader.size = file.size; //get the image's size GB
-		} else {
-			setPreviewIMG('');
-			console.log('No Image Loaded');
-		}
+    if (file) {
+      reader.readAsDataURL(file);
+      reader.name = file.name; //get the image's name
+      reader.size = file.size; //get the image's size GB
+    } else {
+      setPreviewIMG("");
+      console.log("No Image Loaded");
+    }
 
-		reader.onloadend = function () {
-			setPreviewIMG(reader.result);
-			setImgName(reader.name);
-			setImgSize(reader.size);
-		};
-	};
+    reader.onloadend = function () {
+      setPreviewIMG(reader.result);
+      setImgName(reader.name);
+      setImgSize(reader.size);
+    };
+  };
 
-	useEffect(() => {
-		previewIMG &&
-			exifr
-				.parse(`${previewIMG}`, { userComment: true, xmp: true })
-				.then((output) => setDataValue(output));
-	}, [previewIMG]);
+  useEffect(() => {
+    previewIMG &&
+      exifr
+        .parse(`${previewIMG}`, { userComment: true, xmp: true })
+        .then((output) => setDataValue(output));
+  }, [previewIMG]);
 
-	return (
-		<div className='.general__container exif'>
-			<div className='title'>
-				<span>Image Metadata EXIF viewer</span>
-			</div>
+  return (
+    <div className=".general__container exif">
+      <div className="title">
+        <span>Image Metadata EXIF viewer</span>
+      </div>
 
-			<section className='image-picker'>
-				<h1>
-					"The EXIF and XMP metadata, are a set of data that include all kinds
-					of information, describing and providing details about the rights, who
-					created them, when and what use, image size, camera or mobile model,
-					focal length, ISO sensitivity, shutter speed, GPS data and more."
-				</h1>
-				<h3>Images: JPEG, JPG & PNG</h3>
+      <section className="image-picker">
+        <h1>
+          "The EXIF and XMP metadata, are a set of data that include all kinds
+          of information, describing and providing details about the rights, who
+          created them, when and what use, image size, camera or mobile model,
+          focal length, ISO sensitivity, shutter speed, GPS data and more."
+        </h1>
+        <h3>Images: JPEG, JPG & PNG</h3>
 
-				<Share url={'exif.vmog.net'} />
+        <Share url={"exif.vmog.net"} />
 
-				<input
-					id='uploader'
-					type='file'
-					accept='.jpeg, .jpg, .png'
-					onChange={handleInputChange}
-				/>
-				<div className='label-container'>
-					<label htmlFor='uploader' className='btn__click'>
-						<Icon1 />
-						<span>Choose a Image</span>
-					</label>
-				</div>
-				<hr />
+        <input
+          id="uploader"
+          type="file"
+          accept=".jpeg, .jpg, .png"
+          onChange={handleInputChange}
+        />
+        <div className="label-container">
+          <label htmlFor="uploader" className="btn__click">
+            <Icon1 />
+            <span>Select Image</span>
+          </label>
+        </div>
+        <hr />
 
-				<div className='image-container'>
-					{previewIMG && (
-						<pre>
-							<img src={`${img}`} id='img1' alt='File type not supported' />
-							<p>Name: {imgName}</p>
-							{dataValue && (
-								<>
-									<p>
-										Model: {dataValue.Model} Lens: {dataValue.Lens}
-									</p>
-									<p>
-										ISO: {dataValue.ISO} f/{dataValue.FNumber} S: 1/
-										{Math.trunc(1 / dataValue.ExposureTime)} FL:{' '}
-										{dataValue.FocalLength}mm{' '}
-									</p>
-									<p>Size: {Math.trunc(imgSize / 1000)} KB</p>
-								</>
-							)}
-						</pre>
-					)}
-				</div>
+        <div className="image-container">
+          {previewIMG && (
+            <pre>
+              <img src={`${img}`} id="img1" alt="File type not supported" />
+              <p>Name: {imgName}</p>
+              {dataValue && (
+                <>
+                  <p>
+                    Model: {dataValue.Model} Lens: {dataValue.Lens}
+                  </p>
+                  <p>
+                    ISO: {dataValue.ISO} f/{dataValue.FNumber} S: 1/
+                    {Math.trunc(1 / dataValue.ExposureTime)} FL:{" "}
+                    {dataValue.FocalLength}mm{" "}
+                  </p>
+                  <p>Size: {Math.trunc(imgSize / 1000)} KB</p>
+                </>
+              )}
+            </pre>
+          )}
+        </div>
 
-				<div className='data-container'>
-					{previewIMG && (
-						<pre id='allMetaDataPrev' className='text-container'>
-							<p>{JSON.stringify(dataValue, null, '\t')}</p>
-						</pre>
-					)}
-				</div>
-			</section>
-			<footer>
-				<p>
-					{' '}
-					<a href='https://vmog.net/' target='_blank' rel='noreferrer'>
-						Copyright and web design by © VMOG
-					</a>
-				</p>
-				<p> Liverpool UK 2021 </p>
-			</footer>
-		</div>
-	);
+        <div className="link">
+          {previewIMG && (
+            <a
+              href="https://image.vmog.net/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn__link"
+            >
+              Go to RESIZE IMAGE
+            </a>
+          )}
+        </div>
+
+        <div className="data-container">
+          {previewIMG && (
+            <pre id="allMetaDataPrev" className="text-container">
+              <p>{JSON.stringify(dataValue, null, "\t")}</p>
+            </pre>
+          )}
+        </div>
+      </section>
+      <footer>
+        <p>
+          <a href="https://vmog.net/" target="_blank" rel="noreferrer">
+            Copyright and web design by © VMOG
+          </a>
+        </p>
+        <p> Liverpool UK 2021 </p>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
